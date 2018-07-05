25ccdc1dcc967457a036ce17af27b018f0070d72
mashuyi <1763360594@qq.com>
2018-07-05 12:38:49 +0800

    test

---
+++ Design_pattern/Proxy/Proxy.py
@@ -0,0 +1,39 @@
+#encoding=utf-8
+
+# 国内无法访问google,必须通过shadowsock才能访问google
+
+__author__ = 'mashuyi'
+from abc import ABCMeta, abstractmethod
+
+class Google(str):
+	def __init__(self, name):
+		self.name = name
+
+class Access():
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def visit(self):
+		pass
+
+class Home_access(Access):
+	def __init__(self,name,google_A):
+		self.name = name
+		self.google_A = Google(google_A)
+
+	def visit(self):
+	    print("2 *** "+self.name+' *** access *** '+ self.google_A )
+
+class Proxy(Access):
+	def __init__(self, name,proxyed_name,google_A):
+		self.name = name
+		self.proxyed = Home_access(proxyed_name,google_A)
+
+	def visit(self):
+		self.proxyed.visit()
+		print("1 *** "+self.name+" *** access ***" +self.proxyed.google_A)
+
+if __name__ == '__main__':
+	p = Proxy('shadowsock','home','Google')
+	p.visit()
+	
